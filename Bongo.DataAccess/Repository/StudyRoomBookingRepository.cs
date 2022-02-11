2f04e2031aee4c9ff1c2eafd2e16a0077ec5f406
SETAREH <setare_r2000@yahoo.com>
2022-02-11 10:43:54 +0330

    booking project

---
+++ Bongo.DataAccess/Repository/StudyRoomBookingRepository.cs
@@ -0,0 +1,34 @@
+﻿using Bongo.DataAccess.Repository.IRepository;
+using Bongo.Models.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Bongo.DataAccess.Repository
+{
+    public class StudyRoomBookingRepository : IStudyRoomBookingRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public StudyRoomBookingRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+
+        public IEnumerable<StudyRoomBooking> GetAll(DateTime? date)
+        {
+            if (date != null)
+            {
+                return _context.StudyRoomBookings.Where(x => x.Date == date).OrderBy(x => x.BookingId).ToList();
+            }
+            return _context.StudyRoomBookings.OrderBy(x => x.BookingId).ToList();
+        }
+
+        public void Book(StudyRoomBooking booking)
+        {
+            _context.StudyRoomBookings.Add(booking);
+            _context.SaveChanges();
+        }
+    }
+}
